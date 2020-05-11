2e365eac78934ffd13f3e82a84e152102e54dcca
Ludovic Roux <ludovic.rx@eduge.ch>
2020-05-11 16:02:30 +0200

    commencement du tri

--- script/tri.js
+++ script/tri.js
@@ -3,3 +3,48 @@
 // Desc.     :   Système de tri
 // Version   :   1.0, 20.04.2020, LR, version initiale
 
+/**
+ * Trie les âges de manière croissante
+ * @param {array} values valeurs des âges
+ */
+function triAgeCroissant(values) {
+    var isSorted = true;
+
+    do {
+        isSorted = true;
+        for (let index = 0; index < values.length - 1; index++) {
+            if (values[index] > values[index + 1]) {
+                var temp = values[index + 1];
+                values[index + 1] = values[index];
+                values[index] = temp;
+
+                isSorted = false;
+            }
+        }
+    } while (isSorted == false)
+
+    return values;
+}
+
+/**
+ * Trie les âges de manière décroissante
+ * @param {array} values valeurs des âges
+ */
+function triAgeDecroissant(values) {
+    var isSorted = true;
+
+    do {
+        isSorted = true;
+        for (let index = 0; index < values.length - 1; index++) {
+            if (values[index] < values[index + 1]) {
+                var temp = values[index + 1];
+                values[index + 1] = values[index];
+                values[index] = temp;
+
+                isSorted = false;
+            }
+        }
+    } while (isSorted == false)
+
+    return values;
+}
